58f9ab0a07467bd941320d97f006037a9f06bdba
lgabriellp <lgabriellp@e87b1f05-a5fe-3591-f686-b0a1f37f36ed>
2010-05-31 18:32:11 +0000

    Nova Main.

--- WebSipPhone/src/main/WebSipPhone.java
+++ WebSipPhone/src/main/WebSipPhone.java
@@ -1,7 +1,16 @@
 package main;
 
-public class WebSipPhone {
-    public static void main(String[] args) {
-        
+import com.vaadin.ui.Label;
+import com.vaadin.ui.Window;
+
+@SuppressWarnings("serial")
+public class WebSipPhone extends com.vaadin.Application {
+
+    @Override
+    public void init() {
+        final Window main = new Window("Web Sip Phone");
+        setMainWindow(main);
+
+        main.addComponent(new Label("Hello Web Sip Phone"));
     }
 }
