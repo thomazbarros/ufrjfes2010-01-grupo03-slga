daac7bdbf7f36077426376cd16b4d917d0ea3110
lgabriellp <lgabriellp@e87b1f05-a5fe-3591-f686-b0a1f37f36ed>
2010-07-04 15:55:14 +0000

    Redividindo o módulo de controle.

---
+++ WebMessenger/src/control/presence/PresenceManager.java
@@ -0,0 +1,37 @@
+package control.base.signal;
+
+import java.util.Map;
+
+import model.structural.Contact;
+
+public interface PresenceManager {
+    /**
+    *   Gets user own status.
+    *   @return A String object with the user status.
+    */
+    public String getStatus();
+
+    /**
+    *   Sets user own status.
+    *   @param status is the new status of the user.
+    *
+    *   Postcondition:
+    *       The user has an new status.
+    *       User contacts may see this new status.
+    */
+    public void setStatus(String status);
+
+    /**
+    *   Gets user contacts.
+    *   @return A Map object with the user contacts mapped into its status.
+    */
+    public Map<String, Contact> getContacts();
+    
+    /**
+    *   Gets user contacts filtered by name.
+    *   @param str is part of a contact name.
+    *   @return A Map object with the user contacts mapped into its status wich
+    *       theirs email contains str.
+    */
+    public Map<String, Contact> getContacts(String str);
+}
