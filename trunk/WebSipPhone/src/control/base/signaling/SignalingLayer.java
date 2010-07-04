6b16cefee62882ec36ab5ce0305898312725e6a1
lgabriellp <lgabriellp@e87b1f05-a5fe-3591-f686-b0a1f37f36ed>
2010-07-04 13:27:52 +0000

    Limpesa do projeto para reestruturação.

--- trunk/WebSipPhone/src/control/base/signaling/SignalingLayer.java
+++
@@ -1,7 +0,0 @@
-package control.base.signaling;
-
-class SignalingLayer {
-    public void startSession(Peer requester, SignalingListener listener);
-    public void confirmSession(Session session);
-    public void endSession(Session session);
-}
--- trunk/WebSipPhone/src/control/base/signaling/SignalingListener.java
+++
@@ -1,7 +0,0 @@
-package control.base.signaling;
-
-public interface SignalingListener {
-    public void sessionStarted(Session session);
-    public void sessionConfirmed(Session session);
-    public void sessionEnded(Session session);
-}
