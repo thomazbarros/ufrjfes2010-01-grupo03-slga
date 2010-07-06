c3665a147e9d540a74c4592b4c9c1d45ef64c93c
gust.has <gust.has@e87b1f05-a5fe-3591-f686-b0a1f37f36ed>
2010-07-06 12:59:09 +0000

    Adicionada a classe FesChatManager.java

---
+++ WebMessenger/src/control/chat/FesChatManager.java
@@ -0,0 +1,20 @@
+package control.top;
+
+import control.base.ChatListener;
+import control.base.ChatManager;
+
+public class FesChatManager implements ChatManager {
+	
+	@Override
+	public void send(String message) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setChatListener(ChatListener listener) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
