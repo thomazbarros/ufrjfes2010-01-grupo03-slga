dfcc703af68b54625b3ea04b536becc72d7ebda6
leopoldolusquino <leopoldolusquino@e87b1f05-a5fe-3591-f686-b0a1f37f36ed>
2010-07-15 23:04:40 +0000

    alteracao no test do messageservice

--- WebMessenger/src/test/MessageServiceTest.java
+++ WebMessenger/src/test/MessageServiceTest.java
@@ -1,4 +1,45 @@
 package test;
+package control.base;
+
+import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smackx.muc.MultiUserChat;
+
+import control.top.MessageReceiver;
+import control.top.MessageService;
+import control.wrap.XMPPMessageListener;
+
+public class XMPPMessageService implements
+	XMPPMessageListener,
+	MessageService {
+	
+	private MultiUserChat multiUserChat;
+	private String contact;
+	
+	public XMPPMessageService(MultiUserChat multiUserChat, String contact){
+		multiUserChat = this.multiUserChat;
+		contact = this.contact;
+	}
+
+	@Override
+	public void addMessageReceiver(MessageReceiver receiver) {
+		// TODO Auto-generated method stub	
+	}
+
+	@Override
+	public void invite(String contact, String invitationMessage) {
+		multiUserChat.invite(contact, invitationMessage);
+	}
+
+	@Override
+	public void kick(String contact, String reason) throws XMPPException{
+		multiUserChat.kickParticipant(contact, reason);
+	}
+
+	@Override
+	public void send(String message) throws XMPPException {
+		multiUserChat.sendMessage(message);
+	}
+}
 
 import java.util.Collection;
 
@@ -6,7 +47,7 @@ import org.jivesoftware.smackx.muc.Affiliate;
 import org.jivesoftware.smackx.muc.DefaultParticipantStatusListener;
 import org.jivesoftware.smackx.muc.MultiUserChat;
 
-@RunWith(Mock.class)
+@RunWith(JMock.class)
 public class MessageServiceTest{
 	static MessageService service;
 	
