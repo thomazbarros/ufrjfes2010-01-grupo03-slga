52db4c61362660da9fa3f70b859670feb2927d81
leopoldolusquino <leopoldolusquino@e87b1f05-a5fe-3591-f686-b0a1f37f36ed>
2010-07-15 22:48:41 +0000

    alteracao nas interfaces

--- trunk/WebMessenger/src/control/top/MessageService.java
+++
@@ -1,8 +0,0 @@
-package control.top;
-
-public interface MessageService extends MessageSender {
-	public void addMessageReceiver(MessageReceiver receiver);
-	
-	public void invite(String contact);
-	public void kick(String contact);
-}
