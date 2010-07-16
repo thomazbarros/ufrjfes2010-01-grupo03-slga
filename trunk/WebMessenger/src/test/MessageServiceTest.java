a3695f964d106be9ad5ed40d31e43ff338f817f0
leopoldolusquino <leopoldolusquino@e87b1f05-a5fe-3591-f686-b0a1f37f36ed>
2010-07-16 01:12:44 +0000

    alteracao no test do messageservice

--- trunk/WebMessenger/src/test/MessageServiceTest.java
+++ trunk/WebMessenger/src/test/MessageServiceTest.java
@@ -1,66 +1,36 @@
 package test;
 
-import org.jivesoftware.smack.XMPPException;
-import org.jivesoftware.smackx.muc.MultiUserChat;
-
-import control.top.MessageReceiver;
-import control.top.MessageService;
-import control.wrap.XMPPMessageListener;
-
-public class XMPPMessageService implements
-	XMPPMessageListener,
-	MessageService {
-	
-	private MultiUserChat multiUserChat;
-	private String contact;
-	
-	public XMPPMessageService(MultiUserChat multiUserChat, String contact){
-		multiUserChat = this.multiUserChat;
-		contact = this.contact;
-	}
-
-	@Override
-	public void addMessageReceiver(MessageReceiver receiver) {
-		// TODO Auto-generated method stub	
-	}
-
-	@Override
-	public void invite(String contact, String invitationMessage) {
-		multiUserChat.invite(contact, invitationMessage);
-	}
-
-	@Override
-	public void kick(String contact, String reason) throws XMPPException{
-		multiUserChat.kickParticipant(contact, reason);
-	}
-
-	@Override
-	public void send(String message) throws XMPPException {
-		multiUserChat.sendMessage(message);
-	}
-}
-
 import java.util.Collection;
 
+import junit.framework.Assert;
+
+import org.jivesoftware.smack.XMPPException;
 import org.jivesoftware.smackx.muc.Affiliate;
 import org.jivesoftware.smackx.muc.DefaultParticipantStatusListener;
 import org.jivesoftware.smackx.muc.MultiUserChat;
+import org.jmock.integration.junit4.JMock;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import control.base.XMPPMessageService;
+import control.top.MessageService;
 
 @RunWith(JMock.class)
 public class MessageServiceTest{
 	static MessageService service;
+	static MultiUserChat multiUserChat;
+	static String contact;
 	
 	@BeforeClass static public void
 	messageInitiation(){
-		service = new XMPPMessageService();
+		service = new XMPPMessageService(multiUserChat, contact);
 	}
 	
 	@Test public void
-	kick(){
-		MultiUserChat multiuserchat = new MultiUserChat(null, message);
-		Collection<Affiliate> contacts = multiuserchat.getMembers();
-		DefaultParticipantStatusListener participants;
-		String contact;
-		Assert.assertTrue(participants.kicked(contact));
+	kick() throws XMPPException{
+		service.kick(contact, "");
+		Collection<Affiliate> contacts = multiUserChat.getMembers();
+		Assert.assertNull(contacts);
 	}
 }
