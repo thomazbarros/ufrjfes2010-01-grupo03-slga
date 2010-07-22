2493c1acb2c00e8c1dad328e0415f80e56ff03ca
leopoldolusquino <leopoldolusquino@e87b1f05-a5fe-3591-f686-b0a1f37f36ed>
2010-07-22 23:43:24 +0000

    alteracao no test do messageservice

--- WebMessenger/src/test/MessageServiceTest.java
+++ WebMessenger/src/test/MessageServiceTest.java
@@ -4,8 +4,13 @@ import java.util.Collection;
 
 import junit.framework.Assert;
 
+import org.jivesoftware.smack.ConnectionConfiguration;
+import org.jivesoftware.smack.XMPPConnection;
 import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.packet.Message;
 import org.jivesoftware.smackx.muc.Affiliate;
+import org.jivesoftware.smackx.muc.InvitationListener;
+import org.jmock.Expectations;
 import org.jmock.Mockery;
 import org.jmock.integration.junit4.JMock;
 import org.jmock.integration.junit4.JUnit4Mockery;
@@ -15,32 +20,57 @@ import org.junit.runner.RunWith;
 
 import control.base.XMPPConnectionService;
 import control.top.MessageService;
+import control.top.PresenceListener;
+import control.top.PresenceService;
 
 @RunWith(JMock.class)
 public class MessageServiceTest{
 	static MessageService service;
 	static String contact;
-	static XMPPConnectionService connection;
+	static XMPPConnectionService connectionService;
 	static Mockery context;
 	
 	@BeforeClass static public void
 	messageInitiation() throws Exception{
 		contact = "leopoldolusquino@gmail.com";
 		context = new JUnit4Mockery();
-		connection = new XMPPConnectionService();
-		service = connection.chat(contact);
+		connectionService = new XMPPConnectionService();
+		service = connectionService.chat(contact);
 	}
 	
 	@Test public void
-	kick() throws XMPPException{
+	testInvitation(){
+    	final InvitationListener listener = context.mock(InvitationListener.class);
+    	final XMPPConnection connection = null;
+    	
+    	try {
+    		service.invite(contact, "");
+			connectionService.getMultiChat().addInvitationListener(connection, listener);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		context.checking(new Expectations() {{
+			allowing (listener).invitationReceived(
+					connection, 
+					with(any(String.class)),
+					with(any(String.class)),
+					with(any(String.class)),
+					with(any(String.class)),
+					with(any(Message.class)));
+		}});
+    }
+		
+	@Test public void
+	testKick() throws XMPPException{
 		service.kick(contact, "");
-		Collection<Affiliate> contacts = connection.getMultiChat().getMembers();
+		Collection<Affiliate> contacts = connectionService.getMultiChat().getMembers();
 		Assert.assertFalse(contacts.contains(contact));
 	}
 	
 	@Test public void
-	send() throws XMPPException{
+	testSend() throws XMPPException{
 		service.send("Cante e seja feliz!");
-		Assert.assertEquals("Cante e seja feliz!", connection.getMultiChat().nextMessage().toString());
+		Assert.assertEquals("Cante e seja feliz!", connectionService.getMultiChat().nextMessage().toString());
 	}
 }
