a28bd2d9ccf45299db2a1964a850448d09100a1c
gust.has <gust.has@e87b1f05-a5fe-3591-f686-b0a1f37f36ed>
2010-07-28 14:16:30 +0000

    Modificação no nome do pacote

---
+++ trunk/WebMessenger/src/control/base/xmpp/XMPPPresenceService.java
@@ -0,0 +1,61 @@
+package control.base.xmpp;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.TreeSet;
+
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.packet.Presence;
+
+import control.top.PresenceListener;
+import control.top.PresenceService;
+import control.wrap.XMPPPresenceListener;
+
+public class XMPPPresenceService implements
+	PresenceService,
+	XMPPPresenceListener {
+	
+	private XMPPConnection connection;
+	private Set<PresenceListener> listeners;
+	
+	public XMPPPresenceService(XMPPConnection connection) {
+		this.connection = connection;
+		this.listeners = new TreeSet<PresenceListener>();
+	}
+	
+	@Override
+	public void addPresenceListener(PresenceListener listener) {
+		this.listeners.add(listener);
+	}
+
+	@Override
+	public void setStatus(String status) {
+		Presence presence = new Presence(Presence.Type.available);
+	    presence.setStatus(status);
+	    presence.setPriority(24);
+	    presence.setMode(Presence.Mode.available);
+	    connection.sendPacket(presence);
+	}
+
+	@Override
+	public void entriesAdded(Collection<String> entries) {
+		
+	}
+
+	@Override
+	public void entriesDeleted(Collection<String> entries) {
+		
+	}
+
+	@Override
+	public void entriesUpdated(Collection<String> entries) {
+		
+	}
+
+	@Override
+	public void presenceChanged(Presence presence) {
+		for (PresenceListener listener : listeners) {
+			listener.updateEvent(presence.getFrom(), presence.getMode().name());
+		}
+	}
+}
