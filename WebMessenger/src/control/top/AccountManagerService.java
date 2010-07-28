e2caeafc3d3766027b6acadf66ad63cc577509b7
gust.has <gust.has@e87b1f05-a5fe-3591-f686-b0a1f37f36ed>
2010-07-28 14:20:58 +0000

    Modificação na interface para garantir compatibilidade

--- WebMessenger/src/control/top/AccountManagerService.java
+++ WebMessenger/src/control/top/AccountManagerService.java
@@ -1,9 +1,7 @@
 package control.top;
 
-import org.jivesoftware.smack.XMPPException;
-
 public interface AccountManagerService {
-	public void changePassword(String newPassord) throws XMPPException;
-	public void createAccount(String username, String password) throws XMPPException;
-	public void deleteAccount() throws XMPPException;
+	public void changePassword(String newPassord) throws Exception;
+	public void createAccount(String username, String password) throws Exception;
+	public void deleteAccount() throws Exception;
 }
