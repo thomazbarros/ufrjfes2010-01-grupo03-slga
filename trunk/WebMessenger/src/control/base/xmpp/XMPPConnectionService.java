4a4141dba5222ad95b2c8b8037aba9ebfe571417
leopoldolusquino <leopoldolusquino@e87b1f05-a5fe-3591-f686-b0a1f37f36ed>
2010-07-29 11:17:22 +0000

    account

--- trunk/WebMessenger/src/control/base/xmpp/XMPPConnectionService.java
+++ trunk/WebMessenger/src/control/base/xmpp/XMPPConnectionService.java
@@ -26,6 +26,7 @@ public class XMPPConnectionService implements
 	private Map<String, MessageService> chats;
 	private AbstractXML abstractXML;
 	private XMPPContactService contactManagerService;
+	private XMPPAccountService accountManagerService;
 	
 	private void initConnection(String server, int port) {
 		ConnectionConfiguration configuration = new
@@ -34,6 +35,7 @@ public class XMPPConnectionService implements
 		configuration.setCompressionEnabled(true);
 		this.connection = new XMPPConnection(configuration);
 		contactManagerService = new XMPPContactService(connection);
+		accountManagerService = new XMPPAccountService(connection);
 	}
 	
 	public XMPPConnectionService() {
@@ -131,5 +133,9 @@ public class XMPPConnectionService implements
 	
 	public XMPPContactService getContactManagerService() {
 		return contactManagerService;
+	}
+	
+	public XMPPAccountService getAccountManagerService(){
+		return accountManagerService;
 	}	
 }
