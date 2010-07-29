8ed43607b3312700bbed8a714958cc79a321697e
thomazavila <thomazavila@gmail.com>
2010-07-29 08:28:10 +0000

    Readequando o nome da pasta.

---
+++ WebMessenger/src/view/other/MainWindow.java
@@ -0,0 +1,54 @@
+package com.example.asd;
+
+import com.vaadin.ui.Window;
+import com.vaadin.terminal.ExternalResource;
+import com.vaadin.ui.MenuBar;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.MenuBar.Command;
+import com.vaadin.ui.MenuBar.MenuItem;
+
+
+@SuppressWarnings("serial")
+public class MainWindow extends Window{
+	
+	private MessageWindow messageWindow = new MessageWindow();
+    private MenuBar menubar = new MenuBar();
+
+	
+	public MainWindow(String username) {
+		super("WebMessenger");
+		final MenuBar.MenuItem logout = menubar.addItem( "Sair" , menuCommand);
+		final MenuBar.MenuItem contatos = menubar.addItem("Contatos", null);
+               
+        contatos.addItem("Adicionar Contato", menuCommand);
+        contatos.addItem("Remover Contato", menuCommand);
+        contatos.addItem( "Procurar Contato" , menuCommand);
+        contatos.addSeparator();
+
+
+        contatos.addItem("Aqui vai entrar a lista", menuCommand);
+        contatos.addSeparator();
+
+
+        final MenuBar.MenuItem find = menubar.addItem("Busca Google", new Command(){
+            public void menuSelected(MenuItem selectedItem) {
+                getWindow().open(new ExternalResource("http://www.google.com"));
+            }
+        });
+       
+       addComponent(menubar);
+		
+		messageWindow.center();
+		addWindow(messageWindow);
+		
+		messageWindow.startChat("Leopoldo");
+		messageWindow.startChat("Gustavo");
+	}
+	private Command menuCommand = new Command() {
+	        public void menuSelected(MenuItem selectedItem) {
+	            getWindow().showNotification("Action " + selectedItem.getText());
+	        }
+	    };
+
+	
+}
